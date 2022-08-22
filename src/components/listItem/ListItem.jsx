58e2f5bc9da4fd4c63bd9dc0d662d3188d6ca2ed
phantware <phantmoney2011@gmail.com>
2022-08-22 15:10:51 +0100

    listitem component created

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,39 @@
 import './listItem.scss'
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from '@material-ui/icons'
+import { useState } from 'react'
 
 const ListItem = () => {
-  return <div className='listItem'>ListItem</div>
+  return (
+    <div className='listItem'>
+      <img
+        src='https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee'
+        alt=''
+      />
+      <div className='itemInfo'>
+        <div className='icons'>
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+        <div className='itemInfoTop'>
+          <span>1 hour 14 mins</span>
+          <span className='limit'>+18</span>
+          <span>1999</span>
+        </div>
+        <div className='desc'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, amet nostrum.
+        </div>
+        <div className='genre'>Action</div>
+      </div>
+    </div>
+  )
 }
 
 export default ListItem

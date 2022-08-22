6f840f21532ac90153fa5175d5f240a0b1cea151
phantware <phantmoney2011@gmail.com>
2022-08-22 19:38:18 +0100

    listItem component styled

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -7,31 +7,44 @@ import {
 } from '@material-ui/icons'
 import { useState } from 'react'
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHover, setIsHover] = useState(false)
+  const trailer =
+    'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761'
   return (
-    <div className='listItem'>
+    <div
+      className='listItem'
+      style={{ left: isHover && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <img
         src='https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee'
         alt=''
       />
-      <div className='itemInfo'>
-        <div className='icons'>
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownOutlined />
-        </div>
-        <div className='itemInfoTop'>
-          <span>1 hour 14 mins</span>
-          <span className='limit'>+18</span>
-          <span>1999</span>
-        </div>
-        <div className='desc'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Necessitatibus, amet nostrum.
-        </div>
-        <div className='genre'>Action</div>
-      </div>
+      {isHover && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className='itemInfo'>
+            <div className='icons'>
+              <PlayArrow className='icon' />
+              <Add className='icon' />
+              <ThumbUpAltOutlined className='icon' />
+              <ThumbDownOutlined className='icon' />
+            </div>
+            <div className='itemInfoTop'>
+              <span>1 hour 14 mins</span>
+              <span className='limit'>+18</span>
+              <span>1999</span>
+            </div>
+            <div className='desc'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Necessitatibus, amet nostrum.
+            </div>
+            <div className='genre'>Action</div>
+          </div>
+        </>
+      )}
     </div>
   )
 }

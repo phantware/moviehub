fcd57526dbee71c5690c2d069f9f5b1459834232
phantware <phantmoney2011@gmail.com>
2022-08-22 22:21:49 +0100

    login page added and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './app.scss'
-import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+// import Register from './pages/register/Register'
 // import Home from './pages/home/Home'
 // import Watch from './pages/watch/Watch'
 
 function App() {
-  return <Register />
+  return <Login />
 }
 
 export default App

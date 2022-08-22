3533f5dfa998fc945b6d814687949a616f039db7
phantware <phantmoney2011@gmail.com>
2022-08-22 21:59:00 +0100

    useState and useRef added to register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,21 @@
+import { useState, useRef } from 'react'
 import './register.scss'
 
 const Register = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value)
+  }
+
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value)
+  }
+
   return (
     <div className='register'>
       <div className='top'>
@@ -19,10 +34,21 @@ const Register = () => {
         <p>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
-        <div className='input'>
-          <input type='email' placeholder='email address' />
-          <button className='registerButton'>Get Started</button>
-        </div>
+        {!email ? (
+          <div className='input'>
+            <input type='email' placeholder='email address' ref={emailRef} />
+            <button className='registerButton' onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className='input'>
+            <input type='password' placeholder='password ' ref={passwordRef} />
+            <button className='registerButton' onClick={handleFinish}>
+              Started
+            </button>
+          </form>
+        )}
       </div>
     </div>
   )

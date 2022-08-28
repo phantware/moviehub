86276b00d82be1a0dd9bd1a05b49eeba974ba6e2
phantware <phantmoney2011@gmail.com>
2022-08-28 17:57:50 +0100

    checker for if user is login or not added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './app.scss'
-// import Login from './pages/login/Login'
-// import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 function App() {
+  const user = false
   return (
     <Routes>
       <Route exact path='/'>
-        <Route index element={<Home />} />
+        <Route path='/' element={user ? <Home /> : <Register />} />
+        <Route
+          path='/login'
+          element={user ? <Navigate to='/' replace /> : <Login />}
+        />
+        <Route
+          path='/register'
+          element={user ? <Navigate to='/' replace /> : <Register />}
+        />
+
         <Route path='/movies'>
           <Route path='/movies' element={<Home type='movies' />} />
         </Route>
         <Route path='/series'>
           <Route path='/series' element={<Home type='series' />} />
         </Route>
-        <Route index element={<Watch />} />
         <Route path='/watch' element={<Watch />} />
       </Route>
     </Routes>

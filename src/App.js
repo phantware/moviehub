efd0cfc7c06b2af46b520487b05e451804daf6d4
phantware <phantmoney2011@gmail.com>
2022-08-28 22:16:03 +0100

    axios added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch'
 import { Routes, Route, Navigate } from 'react-router-dom'
 function App() {
-  const user = false
+  const user = true
   return (
     <Routes>
       <Route exact path='/'>
@@ -19,13 +19,17 @@ function App() {
           element={user ? <Navigate to='/' replace /> : <Register />}
         />
 
-        <Route path='/movies'>
-          <Route path='/movies' element={<Home type='movies' />} />
-        </Route>
-        <Route path='/series'>
-          <Route path='/series' element={<Home type='series' />} />
-        </Route>
-        <Route path='/watch' element={<Watch />} />
+        {user && (
+          <>
+            <Route path='/movies'>
+              <Route path='/movies' element={<Home type='movies' />} />
+            </Route>
+            <Route path='/series'>
+              <Route path='/series' element={<Home type='series' />} />
+            </Route>
+            <Route path='/watch' element={<Watch />} />
+          </>
+        )}
       </Route>
     </Routes>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
 import Featured from '../../components/featured/Featured'
 import List from '../../components/list/List'
 import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
 
 const Home = ({ type }) => {
+  const [lists, setLists] = useState([])
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  })
   return (
     <div className='home'>
       <Navbar />

bbea293adeec7cf79c64136e1959f5ba43288ce1
phantware <phantmoney2011@gmail.com>
2022-08-28 17:12:20 +0100

    nexted router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './app.scss'
-import Login from './pages/login/Login'
+// import Login from './pages/login/Login'
 // import Register from './pages/register/Register'
-// import Home from './pages/home/Home'
+import Home from './pages/home/Home'
 // import Watch from './pages/watch/Watch'
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
-  return <Login />
+  return (
+    <Routes>
+      <Route exact path='/'>
+        <Route index element={<Home />} />
+        <Route path='/movies'>
+          <Route path='/movies' element={<Home type='movies' />} />
+        </Route>
+        <Route path='/series'>
+          <Route path='/series' element={<Home type='series' />} />
+        </Route>
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
